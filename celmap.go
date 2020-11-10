55dd3c6759131ee91e89ba54ee0cad04f9fff155
Eythan C <eythan@Eythans-MacBook-Pro.local>
2020-11-10 12:36:05 -0700

    initial commit.

---
+++ celmap.go
@@ -0,0 +1,49 @@
+// CelMap (Excel-Map) is used for mapping Excel file cells
+// to and from their string form, and to and from their
+// index form.
+package celmap
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+
+	"github.com/plandem/ooxml"
+)
+
+// GetIndex will get the column and row number for a given Excel cell string.
+// C22 returns 2, 22
+func GetIndex(ref string) (col, row int) {
+	colPart := strings.Map(ooxml.GetLettersFn, ref)
+	rowPart := strings.Map(ooxml.GetNumbersFn, ref)
+
+	var colIndex, rowIndex int
+	for i, j := len(colPart)-1, 0; i >= 0; i, j = i-1, j+1 {
+		colIndex += (int(colPart[i]) - int('A') + 1) * int(math.Pow(26, float64(j)))
+	}
+
+	rowIndex, _ = strconv.Atoi(rowPart)
+
+	colIndex--
+
+	return colIndex, rowIndex
+}
+
+// GetRef will get a cell reference for 0-based col indexes,
+// and 1-based row indexes.
+// 2, 22 returns C22
+func GetRef(col, row int) (ref string) {
+	if col < 0 || row < 0 {
+		return ""
+	}
+
+	var colName string
+	i := col + 1
+	for i > 0 {
+		colName = string(rune(((i-1)%26 + 65))) + colName
+		i = (i - 1) / 26
+	}
+
+	return fmt.Sprintf("%s%d", colName, row)
+}
---
+++ celmap_test.go
@@ -0,0 +1,43 @@
+package celmap
+
+import (
+	"testing"
+)
+
+func TestGetIndex(t *testing.T) {
+	var (
+		expectedFirstCol  = 2
+		expectedFirstRow  = 22
+		expectedSecondCol = 28
+		expectedSecondRow = 1022
+	)
+
+	actualFirstCol, actualFirstRow := GetIndex("C22")
+	actualSecondCol, actualSecondRow := GetIndex("AC1022")
+
+	if actualFirstCol != expectedFirstCol {
+		t.Fatalf("Expected %v for first column but got %v", expectedFirstCol, actualFirstCol)
+	} else if actualFirstRow != expectedFirstRow {
+		t.Fatalf("Expected %v for first row but got %v", expectedFirstRow, actualFirstRow)
+	} else if actualSecondCol != expectedSecondCol {
+		t.Fatalf("Expected %v for second column but got %v", expectedSecondCol, actualSecondCol)
+	} else if actualSecondRow != expectedSecondRow {
+		t.Fatalf("Expected %v for second row but got %v", expectedSecondRow, actualSecondRow)
+	}
+}
+
+func TestGetRef(t *testing.T) {
+	var (
+		expectedFirstString  = "C22"
+		expectedSecondString = "AC1022"
+	)
+
+	actualFirstString := GetRef(2, 22)
+	actualSecondString := GetRef(28, 1022)
+
+	if actualFirstString != expectedFirstString {
+		t.Fatalf("Expected %s for first ref but got %s", expectedFirstString, actualFirstString)
+	} else if actualSecondString != expectedSecondString {
+		t.Fatalf("Expected %v for second ref but got %v", expectedSecondString, actualSecondString)
+	}
+}
